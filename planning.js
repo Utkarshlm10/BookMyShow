f1bf247a66a1985ab8b2d3123d69eb19a80fbea3
Utkarshlm10 <utkarshv2619@gmail.com>
2021-07-25 17:47:39 +0530

    done

---
+++ planning.js
@@ -0,0 +1,23 @@
+// BookMyShow Planning
+
+// -> We need a resuable carousel
+// -> Hero carousel
+// -> Common carousel
+// -> Cast and crew carousel
+
+// Pages
+// -> Home Page
+// -> Movie Page
+// -> Categories Page
+// -> Authorised Page
+// -> RazorPay Gateway
+
+// Plan
+// Layouts
+// Routes
+// Pages
+// Components
+
+// HOC (Higher Order Components)
+// -> it will combine and return a component
+// -> To handel our layout
